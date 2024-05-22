9b848a93546cf87449ba94fb0deb13a9f7e90813
Riwandi <riwandy10@gmail.com>
2024-05-23 04:50:56 +0700

    fix(style): fixed responsive footer

--- src/components/atoms/Footer/index.tsx
+++ src/components/atoms/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='h-full bg-[#323232] text-white p-4 mt-5'>
-      <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3'>
         <div>
           <h4>Visit Us</h4>
           <p>Tangerang, Banten 15710</p>
